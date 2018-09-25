290485144f55ee4b80d9d56d5ea212fbe85f3508
NadiaAiraf <aidanfaria@gmail.com>
2018-09-25 11:00:59 +0100

    so I can see if a question is correct in the game, my Game class will now tell me if a question is correct or incorrect

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,21 @@
 class Game
+  attr_reader :current_question
+
   QUESTIONS = [{'highest mountain in the world?' => {correct: 'everest', wrong: ['kilimanjaro','fiji','k2']}},
                {'the capital of croatia?' => {correct: 'zagreb',wrong: ['london','berlin','moscow']}}]
+  def initialize
+    @current_question = random_question
+  end
+
+  def change_question
+    @current_question = random_question
+  end
+
   def random_question
     QUESTIONS.sample
+  end
+
+  def answer_correct?(answer)
+    current_question.values[0][:correct] == answer
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,19 @@ describe Game do
   describe 'give_question' do
     it 'returns a random question and answer from the list' do
       srand(123)
-      expect(subject.random_question).to eq({"highest mountain in the world?"=>{:correct=>"everest", :wrong=>["kilimanjaro", "fiji", "k2"]}})
+      expect(subject.random_question).to eq({'the capital of croatia?' => {correct: 'zagreb',wrong: ['london','berlin','moscow']}})
+    end
+  end
+
+  describe '#answer_correct?' do
+    it 'returns true if the correct answer is given' do
+      srand(123)
+      expect(subject.answer_correct?('everest')).to eq true
+    end
+
+    it 'returns false if the wrong answer is given' do
+      srand(123)
+      expect(subject.answer_correct?('k2')).to eq false
     end
   end
 end
